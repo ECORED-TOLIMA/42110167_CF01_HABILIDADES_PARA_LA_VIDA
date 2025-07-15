8ce7f50c55a3c4024c4a66ad593fdc930bd62a97
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2025-07-15 18:54:24 -0500

    Ajustes 1

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -37,7 +37,7 @@
       .col-lg-5.mb-lg-0.mb-4(data-aos="fade-right")
         .tarjeta-avatar-b.mb-4
           .tarjeta-avatar-b__img
-            img(src='@/assets/curso/temas/tema5/icoA.svg' alt='AvatarTop')
+            img(src='@/assets/curso/temas/tema5/icoA.svg' alt='Imagen decorativa')
           .tarjeta.BGR01.p-4
             h4.ps-5 Fundantes
             p.ps-5.mb-0 Relacionadas con el ser.
@@ -45,9 +45,9 @@
       .col-lg-5.mb-lg-0.mb-4(data-aos="fade-right")
         .tarjeta-avatar-b
           .tarjeta-avatar-b__img
-            img(src='@/assets/curso/temas/tema5/icoB.svg' alt='AvatarTop')
+            img(src='@/assets/curso/temas/tema5/icoB.svg' alt='Imagen decorativa')
           .tarjeta.BGR01.p-4
-            h4.ps-5 Estratégicas
+            h4.ps-5 Integradoras
             p.ps-5.mb-0 Relacionadas con el hacer.
 
     .bloque-texto-g.bloque-texto-g--inverso.color-primario.mb-5.p-3.p-sm-4.p-md-5(data-aos="fade-down")
@@ -58,7 +58,7 @@
         h4.mb-0 Competencias socioemocionales (CSE)
         br
         br
-        p.mb-0 Son conocimientos, habilidades y destrezas que ayudan a las personas a interactuar con otros, resolver problemas y alcanzar objetivos de forma autónoma y eficaz. Estas incluyen en cómo se piensa, siente y se comunica para enfrentar los retos diarios.
+        p.mb-0 Son conocimientos, habilidades y destrezas que ayudan a las personas a interactuar con otros, resolver problemas y alcanzar objetivos de forma autónoma y eficaz. Estas influyen en cómo se piensa, siente y se comunica para enfrentar los retos diarios.
 
     p.mb-5(data-aos="fade-right") A continuación, se explican dichas categorías:
 
@@ -70,7 +70,7 @@
               img(src='@/assets/curso/temas/tema5/img-1A.png', alt='Imagen decorativa')
           .col-12.col-lg-4
             p.mb-0 Son la base emocional y personal de cada individuo. Incluyen autoestima, resiliencia, regulación emocional y capacidad de comunicación. Estas competencias son esenciales para la conexión con uno mismo, los demás y el mundo.
-      .p-4.p-lg-5(titulo="CSE del hacer (estratégicas)")
+      .p-4.p-lg-5(titulo="CSE del hacer (integradoras)")
         .row.justify-content-center.px-lg-0.px-3
           .col-12.col-lg-8.mb-3
             figure
@@ -92,45 +92,45 @@
               th(colspan="4") Necesidades existenciales
               th(rowspan="2") Necesidades axiológicas
             tr.bg-tb-2
-              th Ser (lo que tiene que asumir para cumplir esa necesidad)
-              th Tener (lo que es necesario poseer para satisfacer esa necesidad)
-              th Hacer (lo que se tiene que realizar para cumplir esa necesidad)
-              th Estar (en dónde se puede cumplir esa necesidad)
+              td #[strong Ser] (Lo que tengo que ser para cumplir esa necesidad) 
+              td #[strong Tener] (Lo que tengo que tener para cumplir esa necesidad)
+              td #[strong Hacer] (Lo que tengo que hacer para cumplir esa necesidad)
+              td #[strong Estar] (En donde puedo cumplir esa necesidad) 
           tbody
             tr
               th Autoestima
-              td Exploración de fortalezas, debilidades, sueños, metas, intereses y aspectos a mejorar, como elementos de reconocimiento en la construcción la trayectoria de vida.
-              td Desarrolla herramientas para priorizar y atender las necesidades de la vida.
-              td Fortalece potencialidades y construye una trayectoria de vida con la cual se alcanza la percepción de realización.
-              td Comparte con personas que permiten fortalecer la autoestima en entornos familiares, sociales, laborales, educativos.
+              td Exploro mis fortalezas, debilidades, sueños, metas, intereses y aspectos a mejorar, como elementos de reconocimiento en la construcción de mi trayectoria de vida.
+              td Desarrollo herramientas para priorizar y atender las necesidades de mi vida.
+              td Fortalezco mis potencialidades y construyo una trayectoria de vida con la que me sienta realizado.
+              td Comparto con personas que me permiten fortalecer mi autoestima en entornos familiares, sociales, laborales, educativos.
               td Afecto, entendimiento, participación, creación, identidad, libertad.
             tr
               th Empatía
-              td Se conecta emocionalmente con el otro entendiendo sus necesidades y puntos de vista.
-              td Desarrolla capacidades para identificar diferentes perspectivas reconociendo al otro, sus pensamientos y sentimientos.
-              td Reconoce los sentimientos y la perspectiva o punto de vista u opinión de otras personas y respeta las diferencias entre lo que cada uno siente respecto a las mismas cosas. 
-              td En los diferentes espacios en los que interactúa, trata de entender los puntos de vista y el sentimiento de los demás.
+              td Me conecto emocionalmente con el otro entendiendo sus necesidades y puntos de vista.
+              td Desarrollo capacidades para identificar diferentes perspectivas reconociendo al otro, sus pensamientos y sentimientos.
+              td Reconozco los sentimientos y la perspectiva o punto de vista u opinión de otras personas y respeto las diferencias entre lo que cada uno siente respecto a las mismas cosas.
+              td En los diferentes espacios en los que interactúo, trato de entender los puntos de vista y el sentimiento de los demás.
               td Afecto, entendimiento, identidad, participación, libertad.
             tr
               th Regulación emocional
-              td Desarrollo de procesos de autoconocimiento que permiten identificar emociones y la manera de regularlas.
-              td Utilización herramientas de identificación y manejo de emociones en la vida personal y en la interacción con los otros.
-              td Aprende a ejercen una influencia sobre las emociones que experimento, sobre cuando suceden y sobre cómo se experimentan y se expresan.
-              td En los espacios en los que participo, se promueve la expresión y manejo de las emociones individuales y colectivas, de una manera constructiva.
+              td Desarrollo procesos de autoconocimiento que me permitan identificar mis emociones y la manera en que las puedo regular.
+              td Utilizo herramientas de identificación y manejo de emociones en la vida personal y en la interacción con los otros.
+              td Aprendo a ejercen una influencia sobre las emociones que experimento, sobre cuando suceden y sobre cómo se experimentan y se expresan.
+              td En los espacios en los que participo, promuevo la expresión y manejo de las emociones individuales y colectivas, de una manera constructiva.
               td Subsistencia, protección, afecto, participación, identidad, libertad.
             tr
               th Resiliencia
-              td Identificación de las características que permiten superar las situaciones difíciles. 
-              td Reconoce los mecanismos que permiten superar la adversidad.
-              td Poner en práctica acciones que potencien fortalezas y me ayuden a superar las adversidades. 
-              td Creación de espacios con el entorno familiar y de amistad, que permitan consolidar percepciones optimistas del futuro. 
+              td Identifico en mi la característica que me han permitido superar las situaciones difíciles.
+              td Reconozco los mecanismos que me han permitido superar la adversidad a mí y a otros.
+              td Pongo en práctica acciones que potencien mis fortalezas y me ayuden a superar las adversidades.
+              td Creo espacios con mi entorno familiar y mis amigos, que me permitan consolidar percepciones optimistas del futuro.
               td Subsistencia, protección, participación, identidad.
             tr
-              th Comunicación 
-              td Identificación de habilidades para intercambiar información con otros con el fin de comunicar pensamientos, sentimientos y opiniones. 
-              td Utilización de formas de comunicación basadas en el respeto y control emocional que permiten exponer claramente ideas y puntos de vista. 
-              td Aprender a expresarse verbal y no verbalmente en la interacción con el otro.
-              td Creación y participación en espacios para intercambiar opiniones de una manera asertiva.
+              th Comunicación
+              td Identifico mis habilidades para intercambiar información con otros con el fin de comunicar mis pensamientos, sentimientos y opiniones.
+              td Uso formas de comunicación basadas en el respeto y control emocional que me permiten exponer claramente mis ideas y puntos de vista.
+              td Aprendo a expresarme verbal y no verbalmente en la interacción con el otro.
+              td Creo y participo en espacios donde puedo intercambiar opiniones de una manera asertiva.
               td Afecto, entendimiento, participación, identidad.
 
 
@@ -142,7 +142,7 @@
       .col-8.col-lg-4.order-lg-2.order-1.mb-5.mb-lg-0
         img(src='@/assets/curso/temas/tema5/img-3.png', alt='Imagen decorativa')
 
-    p.mb-5(data-aos="fade-right") Y como segunda categoría, hacen parte de las competencias estratégicas: liderazgo, gestión del conflicto, trabajo en equipo y toma de decisiones, las cuales se explican a continuación:
+    p.mb-5(data-aos="fade-right") Y como segunda categoría, hacen parte de las competencias integradoras: liderazgo, gestión del conflicto, trabajo en equipo y toma de decisiones, las cuales se explican a continuación:
 
 
     .row.justify-content-center.p-4(data-aos="zoom-in-right")
@@ -152,7 +152,7 @@
         table(alt="Tabla que relaciona las competencias estratégicas y sus necesidades existenciales y axiológicas.")
           thead
             tr.text-center
-              th(rowspan="2") Competencias estratégicas
+              th(rowspan="2") Competencias integradoras 
               th(colspan="4") Necesidades existenciales
               th(rowspan="2") Necesidades axiológicas
             tr.bg-tb-2
@@ -162,34 +162,34 @@
               th Estar
           tbody
             tr
-              th Liderazgo 
-              td La persona se empodera de sus deseos, sueños y anhelos, planteando su trayectoria de vida como un ser transformador y visionario.
-              td Desarrolla capacidades para relacionarse con otras personas y actuar como agente de cambio.
-              td Genera procesos de transformación del entorno desde acciones personales y colectivas.
+              th Liderazgo
+              td Me empodero de mis deseos, sueños, anhelos y planteó mi trayectoria de vida como ser transformador y visionario. 
+              td Desarrollo capacidades para relacionarme con otras personas y actuar como agente de cambio.  
+              td Genero procesos de transformación de mi entorno desde acciones personales y colectivas.
               td 
-                p Promueve y participa en escenarios de empoderamiento individual y colectivo, siendo 
-                p Corresponsable en la transformación de mi entorno.
+                p Promuevo y participo en escenarios de empoderamiento individual y colectivo, siendo. 
+                p.mb-0 Corresponsable en la transformación de mi entorno.
               td Entendimiento, participación, creación, libertad.
             tr
               th Gestión del conflicto
-              td Potencia la capacidad de diálogo, desde la tolerancia y la identiﬁcación de las múltiples perspectivas de interpretar el mundo, en las que priman el reconocimiento de los derechos del otro.
-              td Reconoce las fuentes de conﬂicto y sus efectos en la convivencia, para aprender a partir de la experiencia en el relacionamiento con los demás.
-              td Realiza acciones de promoción de la convivencia pacífica y prevención de violencia, mejorando la capacidad de respuesta ante escenarios desafiantes.
-              td Construye espacios de vida libres de estrés y violencia, con estrategias alternas para manejo de desacuerdos y conﬂictos.
+              td Potencio mi capacidad de diálogo, desde la tolerancia y la identificación de las múltiples perspectivas de interpretar el mundo, en las que priman el reconocimiento de los derechos del otro. 
+              td Reconozco las fuentes de conflicto y sus efectos en la convivencia, para aprender a partir de la experiencia en el relacionamiento con los demás. 
+              td Realizo acciones de promoción de la convivencia pacífica y prevención de violencia, mejorando mi capacidad de respuesta ante escenarios desafiantes.
+              td Construyo espacios de vida libres de estrés y violencia, con estrategias alternas para manejo de desacuerdos y conﬂictos.
               td Participación, entendimiento, creación, libertad.
             tr
               th Trabajo en equipo
-              td Fortalece el pensamiento creativo y capacidad de adaptación, para responder de manera flexible y responsable en actividades y funciones con diferentes personas en búsqueda de un propósito común.
-              td Reconoce la importancia del aporte al cumplimiento de los objetivos planteados desde diferentes roles que puede asumir, en los distintos contextos en los que se desenvuelve. 
-              td Asume rol en las tareas que son encomendadas como integrante en procesos colaborativos y de trabajo.
-              td Participa en escenarios que permiten relacionarse de manera colaborativa con otros y aportar al cumplimiento de objetivos comunes.
-              td Participación, entendimiento, creación, libertad. 
+              td Fortalezco mi pensamiento creativo y capacidad de adaptación, para responder de manera flexible y responsable en actividades y funciones con diferentes personas en búsqueda de un propósito común. 
+              td Reconozco la importancia de mi aporte al cumplimiento de los objetivos planteados desde diferentes roles que puedo asumir, en los distintos contextos en los que me desenvuelvo.  
+              td Asumo mi rol en las tareas que me son encomendadas como integrante en procesos colaborativos y de trabajo.
+              td Participo en escenarios que me permiten relacionarme de manera colaborativa con otros y aportar al cumplimiento de objetivos comunes.
+              td Participación, entendimiento, creación, libertad.
             tr
               th Toma de decisiones
-              td Reconoce la importancia de escucharse a sí mismo y a las personas en quienes confía para elegir la opción más adecuada en su vida personal y comunitaria.
-              td Realiza ejercicios de reflexión individual y colectiva que le permitan contribuir a la definición de alternativas de acción.
-              td Implementa acciones que me permiten analizar situaciones y elegir la opción más adecuada.
-              td Hace parte de escenarios en los que puede contribuir en decisiones que afectan de forma individual o colectiva.
+              td Reconozco la importancia de escucharme y escuchar a las personas en las que confió para elegir la opción más adecuada en mi vida personal y comunitaria.  
+              td Realizo ejercicios de reﬂexión individual y colectiva que me permitan aportar en la definición de alternativas de acción. 
+              td Implemento acciones que me permiten analizar situaciones y elegir la opción más adecuada.
+              td Hago parte de escenarios en los que puedo contribuir en decisiones que me afectan de manera individual o colectiva.
               td Participación, entendimiento, creación, libertad.
 
 
