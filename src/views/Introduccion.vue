38662f95df7ddb8a955d380a0d4267b85759aac6
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2025-07-18 15:57:33 -0500

    Ajustes 3

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -13,7 +13,7 @@
       .col-lg-5.col-10.mb-4.mb-lg-0(data-aos="fade-right")
         img(src='@/assets/curso/temas/img_1.png', alt='')
       .col-lg-5(data-aos="fade-left")
-        p.mb-3 En este componente formativo se desarrolla el enfoque Desarrollo a Escala Humana (DEH), como una manera de ver el desarrollo y siendo el protagonista el ser humano y la satisfacción de sus necesidades humanas fundamentales, lo cual es basado en Max-Neef, Elizalde & Hopenhayn (1986); concibiéndolo como un ser integral que está en una continua persecución de equilibrio entre lo espiritual, lo psicológico, lo físico y su interacción permanente consigo mismo y con su entorno.
+        p.mb-3 En este componente formativo se presenta el enfoque Desarrollo a Escala Humana (DEH), como una manera de ver el desarrollo y siendo el protagonista el ser humano y la satisfacción de sus necesidades humanas fundamentales, lo cual es basado en Max-Neef, Elizalde & Hopenhayn (1986); concibiéndolo como un ser integral que está en una continua persecución de equilibrio entre lo espiritual, lo psicológico, lo físico y su interacción permanente consigo mismo y con su entorno.
         p.mb-0 Lo anterior, lo complementa Savater (1991), al indicar que en el devenir de la vida, el humano continuamente se enfrenta a diferentes condiciones que generan emociones y comportamientos que llevan a escoger entre diversas opciones, las cuales están enmarcadas en los valores y concepciones adquiridas en la experiencia, del deber ser, de la cultura, la familia y el contexto en el que está involucrado, por lo que tendrá que discernir entre lo “bueno y lo malo”, “lo correcto e incorrecto”; así como afrontar situaciones difíciles que pondrán a prueba su resiliencia y capacidad para sobreponerse y encontrar las fortalezas personales que le permitirán salir adelante. 
 
     p.mb-3(data-aos="fade-left") Es por esto, que se requiere el desarrollo de competencias que permitan la mejora personal en cuanto a la convivencia consigo mismo y con el otro, donde se desarrolle empatía, resiliencia, autoestima y pensamientos positivos, se aprenda a trabajar en equipo, gestionar conflictos, tomar decisiones que permitan ver soluciones y propuestas de mejora para sí mismo y para poder crear sociedad.
@@ -22,7 +22,7 @@
 
     figure.mb-5(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/8DU5V_30RYg" title="Fundamentos del enfoque Desarrollo a Escala Humana" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/eoo6f08n6Tw?si=pYxl1IBwwuusfX55" title="Fundamentos del enfoque Desarrollo a Escala Humana" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     
 </template>
